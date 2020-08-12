1d727a055e75ddcf9c6b1ef8c87de035e608974b
gabrielscorehero <gabrielscorehero55@gmail.com>
2020-08-12 12:53:02 -0300

    created splash screen

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final Color colorBlueVisible = Color(0xFF34B7F1);
+final Color chatBackground = Color(0xFFDCF8C6);
+final Color lightGreen = Color(0xFF25D366);
